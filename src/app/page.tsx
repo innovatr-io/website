4daf23032c72794c9c251b52344274be2a832250
Jo Vinkenroye <jestersimpps@gmail.com>
2024-09-29 16:24:48 +0200

    feat: add GradientBanner to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Layout from "./components/Layout";
+import { GradientBanner } from "./components/GradientBanner";
 
 export default function Home() {
  return (
-  <Layout>
-   {/* Content will be added here */}
-  </Layout>
+  <div className="relative min-h-screen">
+    <GradientBanner className="absolute inset-0 z-0" />
+    <Layout className="relative z-10">
+      {/* Content will be added here */}
+    </Layout>
+  </div>
  );
 }
