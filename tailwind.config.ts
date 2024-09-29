5122615af444663871e0efa019a0175135030bbb
Jo Vinkenroye <jestersimpps@gmail.com>
2024-09-29 11:55:09 +0200

    feat: add daisyui plugin to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,23 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+ content: [
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+ ],
+ theme: {
+  extend: {
+   colors: {
+    background: "var(--background)",
+    foreground: "var(--foreground)",
+   },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
+ },
+ plugins: [daisyui],
+ daisyui: {
+  themes: ["light", "dark"],
+ },
 };
 export default config;
