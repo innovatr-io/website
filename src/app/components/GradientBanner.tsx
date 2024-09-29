bcd103d03dc922af8abe9ee1695cc80e903de2ac
Jo Vinkenroye <jestersimpps@gmail.com>
2024-09-29 15:56:10 +0200

    feat: Improve gradient banner on about page

--- src/app/components/GradientBanner.tsx
+++ src/app/components/GradientBanner.tsx
@@ -14,23 +14,20 @@ export const GradientBanner = ({
     <div className={cn("w-full h-[400px] relative flex flex-col items-center justify-center overflow-hidden", className)}>
       <div className="absolute inset-0 w-full h-full bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
       <div className="absolute inset-0 w-full h-full">
-        <div className="absolute top-0 left-0 right-0 bottom-16 bg-gradient-to-r from-cosmos-primary to-cosmos-secondary opacity-0"></div>
-        <div className="absolute top-0 left-8 right-8 bottom-8 bg-gradient-to-r from-cosmos-secondary to-cosmos-accent opacity-0"></div>
-        <div className="absolute top-8 left-0 right-0 bottom-0 bg-gradient-to-r from-cosmos-accent to-cosmos-primary opacity-0"></div>
         <motion.div
           animate={{
-            opacity: [0, .5, 0],
+            opacity: [0, 0.5, 0],
           }}
           transition={{
             repeat: Infinity,
             duration: 5,
             ease: "easeInOut",
           }}
-          className="absolute inset-0 bg-gradient-to-r from-cosmos-primary to-cosmos-secondary"
+          className="absolute inset-0 bg-gradient-to-br from-cosmos-primary via-cosmos-secondary to-cosmos-accent"
         />
         <motion.div
           animate={{
-            opacity: [0, .2, 0],
+            opacity: [0, 0.3, 0],
           }}
           transition={{
             repeat: Infinity,
@@ -38,21 +35,10 @@ export const GradientBanner = ({
             ease: "easeInOut",
             delay: 2,
           }}
-          className="absolute inset-0 bg-gradient-to-r from-cosmos-secondary to-cosmos-accent"
-        />
-        <motion.div
-          animate={{
-            opacity: [0, .7, 0],
-          }}
-          transition={{
-            repeat: Infinity,
-            duration: 5,
-            ease: "easeInOut",
-            delay: 4,
-          }}
-          className="absolute inset-0 bg-gradient-to-r from-cosmos-accent to-cosmos-primary"
+          className="absolute inset-0 bg-gradient-to-tr from-cosmos-accent via-cosmos-secondary to-cosmos-primary"
         />
       </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-cosmos-background"></div>
       <div className="relative z-20 flex flex-col items-center justify-center text-center">
         {children}
       </div>
