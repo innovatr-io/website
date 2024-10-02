d8c6ee7c75ca6d074311487b1415ed4174b9dc22
Jo Vinkenroye <jestersimpps@gmail.com>
2024-10-02 23:46:07 +0200

    feat: add sparkles effect to the hero section on the landing page

--- src/app/sections/AboutHeroSection.tsx
+++ src/app/sections/AboutHeroSection.tsx
@@ -2,19 +2,29 @@
 
 import { motion } from "framer-motion";
 import { TextGenerateEffect } from "../components/TextGenerateEffect";
+import { SparklesCore } from "../components/SparklesCore";
 
 export const AboutHeroSection = () => {
  return (
-  <section className="w-full flex flex-col items-center justify-center text-center py-24 px-4 sm:px-6 lg:px-8 mt-16">
+  <section className="w-full flex flex-col items-center justify-center text-center py-24 px-4 sm:px-6 lg:px-8 mt-16 relative overflow-hidden">
+   <SparklesCore
+    id="tsparticles"
+    background="transparent"
+    minSize={0.6}
+    maxSize={1.4}
+    particleColor="#5EE2E7"
+    particleDensity={100}
+    className="absolute inset-0 z-0"
+   />
    <motion.h1
     initial={{ opacity: 0, y: -50 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.8, ease: "easeOut" }}
-    className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6 text-white "
+    className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6 text-white relative z-10"
    >
     Revolutionizing Ownership
    </motion.h1>
-   <p className="text-2xl mb-8 text-white drop-shadow-md">
+   <p className="text-2xl mb-8 text-white drop-shadow-md relative z-10">
     <TextGenerateEffect words="A unique community driven fractional ownership platform" />
    </p>
   </section>
