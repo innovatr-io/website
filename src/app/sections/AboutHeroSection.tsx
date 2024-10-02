572dabc0dac3636904e19570c74ea85caf9df941
Jo Vinkenroye <jestersimpps@gmail.com>
2024-10-03 00:05:23 +0200

    feat: make sparkles in the hero section full width

--- src/app/sections/AboutHeroSection.tsx
+++ src/app/sections/AboutHeroSection.tsx
@@ -7,16 +7,18 @@ import { SparklesCore } from "../components/SparklesCore";
 export const AboutHeroSection = () => {
  return (
   <section className="w-full flex flex-col items-center justify-center text-center py-24 px-4 sm:px-6 lg:px-8 mt-16 relative overflow-hidden">
-   <SparklesCore
-    id="tsparticles"
-    background="transparent"
-    minSize={0.3}
-    maxSize={2}
-    particleColor="#5EE2E7"
-    particleDensity={50}
-    speed={0.4}
-    className="absolute inset-0 z-0"
-   />
+   <div className="absolute inset-0 w-screen">
+    <SparklesCore
+     id="tsparticles"
+     background="transparent"
+     minSize={0.3}
+     maxSize={2}
+     particleColor="#5EE2E7"
+     particleDensity={50}
+     speed={0.4}
+     className="w-full h-full"
+    />
+   </div>
    <motion.h1
     initial={{ opacity: 0, y: -50 }}
     animate={{ opacity: 1, y: 0 }}
