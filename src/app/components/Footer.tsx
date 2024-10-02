fdbad4a596398136dbe1e81788bcae791a3155a1
Jo Vinkenroye <jestersimpps@gmail.com>
2024-10-02 23:21:27 +0200

    fix: update Facebook icon color for consistency with other social media icons

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
           <div className="w-full md:w-1/4">
             <h3 className="text-lg font-semibold mb-2">Follow Us</h3>
             <div className="flex space-x-4">
-              <a href="https://www.facebook.com/profile.php?id=61553927551050" target="_blank" rel="noopener noreferrer" className="text-cosmos-text-light/80 hover:text-cosmos-accent">
+              <a href="https://www.facebook.com/profile.php?id=61553927551050" target="_blank" rel="noopener noreferrer" className="text-cosmos-text-gray hover:text-cosmos-accent">
                 <span className="sr-only">Facebook</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
