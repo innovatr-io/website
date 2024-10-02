71424f27970b9a01f53d751a2ee92c88209901fe
Jo Vinkenroye <jestersimpps@gmail.com>
2024-10-02 23:40:37 +0200

    feat: update MusicIcon to a more visually appealing design

--- src/app/components/Icons.tsx
+++ src/app/components/Icons.tsx
@@ -32,7 +32,7 @@ export const ArtIcon: React.FC<IconProps> = (props) => (
 
 export const MusicIcon: React.FC<IconProps> = (props) => (
   <IconWrapper {...props}>
-    <path d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
+    <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6zm-2 16c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
   </IconWrapper>
 );
 
